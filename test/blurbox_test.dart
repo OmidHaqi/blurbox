f76378954b2e414d3ae19198a96b7299b7bacbe4
Umut <omid.haqi@outlook.com>
2024-03-26 22:00:50 +0330

    fix const issues

--- test/blurbox_test.dart
+++ test/blurbox_test.dart
@@ -6,8 +6,8 @@ import 'package:blurbox/blurbox.dart';
 void main() {
   testWidgets('BlurBox should render a blurred child widget', (WidgetTester tester) async {
     // Arrange: Create a BlurBox with a child widget
-    final childWidget = Text('Hello, world!');
-    await tester.pumpWidget(BlurBox(child: childWidget));
+    const childWidget = Text('Hello, world!');
+    await tester.pumpWidget(const BlurBox(child: childWidget));
 
     // Assert: Verify that the blurred child widget is rendered correctly
     expect(find.text('Hello, world!'), findsOneWidget);
